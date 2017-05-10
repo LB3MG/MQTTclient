7548468d302a271b31d19cf8c44bad4b37579b2f
LB3MG <lb3mg@radiokameratene.no>
2017-05-10 12:27:19 +0200

    first
    first

---
+++ MQTTclient.ino
@@ -0,0 +1,184 @@
+
+/*
+ * ESP8266 code for Sonoff relay.
+ * Maintained by lb3mg@radiokameratene.no
+ * 
+ * It connects to an MQTT server then:
+ * on 0 switches off relay
+ * on 1 switches on relay
+ * on 2 switches the state of the relay
+ * publishes 0 once on outTopic when off
+ * publishes 1 once on outTopic when on
+ * 
+ * It will reconnect to the server if the connection is lost using a blocking
+ * reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ * achieve the same result without blocking the main loop.
+ * 
+ * The current state is stored in EEPROM and restored on bootup.
+ * Thanks to rutierut on the esp8266.com forum for the examples.
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Bounce2.h>
+#include <EEPROM.h>
+
+// Network setup
+const char* ssid = "Link";              //Your APs SSID
+const char* password = "abcd1234";      //The password for it
+const char* mqtt_server = "10.0.0.11";  //The IP of your broker
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+// Topics for MQTT
+const char* outTopic = "espout"; //publish
+const char* inTopic = "espin"; //subscribe
+
+int relay_pin = 12; //12 on sonoff maybe
+int button_pin = 0; //The button on the Sonoff is on GPIO 0
+bool relayState = LOW; //State on boot
+
+// Instantiate a Bounce object :
+Bounce debouncer = Bounce(); 
+
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    extButton();
+    for(int i = 0; i<500; i++){
+      extButton();
+      delay(1);
+    }
+    Serial.print(".");
+  }
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '0') {
+    digitalWrite(relay_pin, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    Serial.println("relay_pin -> LOW");
+    relayState = LOW;
+    EEPROM.write(0, relayState);    // Write state to EEPROM
+    EEPROM.commit();
+  } else if ((char)payload[0] == '1') {
+    digitalWrite(relay_pin, HIGH);  // Turn the LED off by making the voltage HIGH
+    Serial.println("relay_pin -> HIGH");
+    relayState = HIGH;
+    EEPROM.write(0, relayState);    // Write state to EEPROM
+    EEPROM.commit();
+  } else if ((char)payload[0] == '2') {
+    relayState = !relayState;
+    digitalWrite(relay_pin, relayState);  // Turn the LED off by making the voltage HIGH
+    Serial.print("relay_pin -> switched to ");
+    Serial.println(relayState); 
+    EEPROM.write(0, relayState);    // Write state to EEPROM
+    EEPROM.commit();
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(outTopic, "Sonoff1 booted");
+      // ... and resubscribe
+      client.subscribe(inTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      for(int i = 0; i<5000; i++){
+        extButton();
+        delay(1);
+      }
+    }
+  }
+}
+
+void extButton() {
+  debouncer.update();
+   
+   // Call code if Bounce fell (transition from HIGH to LOW) :
+   if ( debouncer.fell() ) {
+     Serial.println("Debouncer fell");
+     // Toggle relay state :
+     relayState = !relayState;
+     digitalWrite(relay_pin,relayState);
+     EEPROM.write(0, relayState);    // Write state to EEPROM
+     if (relayState == 1){
+      client.publish(outTopic, "1");
+     }
+     else if (relayState == 0){
+      client.publish(outTopic, "0");
+     }
+   }
+}
+
+void setup() {
+  EEPROM.begin(512);              // Begin eeprom to store on/off state
+  pinMode(relay_pin, OUTPUT);     // Initialize the relay pin as an output
+  pinMode(button_pin, INPUT);     // Initialize the relay pin as an output
+  pinMode(13, OUTPUT);
+  relayState = EEPROM.read(0);
+  digitalWrite(relay_pin,relayState);
+  
+  debouncer.attach(button_pin);   // Use the bounce2 library to debounce the built in button
+  debouncer.interval(50);         // Input must be low for 50 ms
+  
+  digitalWrite(13, LOW);          // Blink to indicate setup
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(500);
+  
+  Serial.begin(115200);
+  setup_wifi();                   // Connect to wifi 
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  extButton();
+}
